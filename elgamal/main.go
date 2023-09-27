3a234c0729fe4820b28f316358b36b2f193cac5b
nselpriv <nicolaiselo@gmail.com>
2023-09-27 17:42:45 +0200

    small things

--- elgamal/main.go
+++ elgamal/main.go
@@ -3,28 +3,36 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"math/rand"
 )
 
 
 type values struct {
-	public_shared_base big.Int
-	public_shared_prime big.Int
-	BOB_public_key big.Int
+	public_shared_base big.Int //g
+	public_shared_prime big.Int //p
+	BOB_public_key big.Int //g^x mod p
 }
 
 
 func main() {
 	message:= "’2000’."
 
-	vals:=values{
+	vals:=&values{
 		public_shared_base: *big.NewInt(666),
 		public_shared_prime: *big.NewInt(6661),
 		BOB_public_key: *big.NewInt(2227),
 	}
 	
+	elgamal(vals, message)
+}
 
-
-
-	fmt.Println(message)
-	fmt.Println(vals.public_shared_base )
+func elgamal(v *values, s string){
+	//first we select a random y for alice
+	
+	r := rand.Intn(6661 - 666) + 666
+	bigR := big.NewInt(int64(r))
+	fmt.Print(r)
+	
+	fmt.Println(new(big.Int).Exp(bigR, &v.BOB_public_key, &v.public_shared_base))
+	
 }
